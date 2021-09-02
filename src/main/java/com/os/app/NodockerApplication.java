d33586e0a7526a907da37d0d62cd1a6f8303c5b1
koushik <sarkar_koushik@yahoo.com>
2021-09-02 13:16:48 -0500

    initial commit

---
+++ src/main/java/com/os/app/NodockerApplication.java
@@ -0,0 +1,13 @@
+package com.os.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NodockerApplication.class, args);
+	}
+
+}
